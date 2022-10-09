e9c329d2c1205bc8ffe1cb1b172e192a00fc008e
Rakibul Islam <rakibul241221@gmail.com>
2022-10-09 22:39:36 +0600

    conditional rendering

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handleRemoveItem }) => {
     let message;
@@ -17,13 +18,17 @@ const Cart = ({ cart, handleRemoveItem }) => {
     }
     return (
         <div>
-            <h2>Orders Summary</h2>
-            <h5>Orders quantity:  {cart.length}</h5>
+            <h2 className={cart.length === 2 ? 'orange' : 'purple'}>Orders Summary</h2>
+            <h5 className={`bold ${cart.length === 2 ? 'blue' : 'orange'}`}>Orders quantity:  {cart.length}</h5>
             {cart.map(ts => <p key={ts._id}>
                 {ts.name} <button onClick={() => handleRemoveItem(ts)}>X</button></p>)}
             {message}
-
+            <p>Ternary operator</p>
             {cart.length === 3 ? <p>Tin jonke Gift Korba?</p> : <p>Kino Kino R o Kinooo!!</p>}
+            <p>&& operator</p>
+            {cart.length === 2 && <h6>tmi 2ta item kinna felcho!!</h6>}
+            <h5>|| operator</h5>
+            {cart.length === 4 || <p>charta items na!!</p>}
         </div>
     );
 };
@@ -35,5 +40,7 @@ conditinal rendering
 
 1. use element in a variable and then use if-else to set value  or
 2. ternary operation condition
+3. && operator (if condition is true i want to display something)
+4. || or operator (if condition is false i want display something)
 
 */
